2318daf55f9f9f05f7c0be3b5d195a748526f966
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-10-26 16:46:03 +0530

    fsdasdsef

--- src/Component/Todolist.jsx
+++ src/Component/Todolist.jsx
@@ -76,7 +76,7 @@ const TodoList = () => {
   return (
     <div className="todo-container">
       <h1>Todo List</h1>
-      {/* <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={inputValue}
@@ -84,8 +84,8 @@ const TodoList = () => {
           placeholder="Enter your todo..."
         />
         <button type="submit">{editIndex !== null ? 'Update' : 'Add'}</button>
-      </form> */}
-      {/* <ul className="todo-list">
+      </form>
+      <ul className="todo-list">
         {todos.map((todo, index) => (
           <li key={index}>
            <input type="check" onClick={()=>handleCheck(todo._id)}/> {todo.text}
@@ -95,7 +95,7 @@ const TodoList = () => {
             </div>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 };
