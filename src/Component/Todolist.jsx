ebc8674a64a816613b1f6015074b8c7b639d886a
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-10-26 16:39:39 +0530

    trgr

--- src/Component/Todolist.jsx
+++ src/Component/Todolist.jsx
@@ -5,73 +5,73 @@ import { useEffect } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-  const [editIndex, setEditIndex] = useState(null);
-  useEffect(()=>{
-    getData();
-  })
-  const getData = async()=>{
+  // const [todos, setTodos] = useState([]);
+  // const [inputValue, setInputValue] = useState('');
+  // const [editIndex, setEditIndex] = useState(null);
+  // useEffect(()=>{
+  //   getData();
+  // })
+  // const getData = async()=>{
      
 
-     const response = await axios.get('https://w3dev-assignment-backend.vercel.app/todo', {
-         headers: {
-             Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
-         }
+  //    const response = await axios.get('https://w3dev-assignment-backend.vercel.app/todo', {
+  //        headers: {
+  //            Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
+  //        }
          
-     });
+  //    });
 
      
-   console.log(response.data.message)
-   setTodos(response.data.message)
-  }
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+  //  console.log(response.data.message)
+  //  setTodos(response.data.message)
+  // }
+  // const handleInputChange = (event) => {
+  //   setInputValue(event.target.value);
+  // };
 
-  const handleSubmit = async(event) => {
-    event.preventDefault();
-    if (editIndex !== null) {
+  // const handleSubmit = async(event) => {
+  //   event.preventDefault();
+  //   if (editIndex !== null) {
       
-    const response = await axios.put(`https://w3dev-assignment-backend.vercel.app/todo/${editIndex}?updateData=${inputValue}`,{}, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
-        }
-    });
-      alert(response.data.message)
-      setEditIndex(null);
-    }else{
+  //   const response = await axios.put(`https://w3dev-assignment-backend.vercel.app/todo/${editIndex}?updateData=${inputValue}`,{}, {
+  //       headers: {
+  //           Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
+  //       }
+  //   });
+  //     alert(response.data.message)
+  //     setEditIndex(null);
+  //   }else{
       
-      const response = await axios.post(`https://w3dev-assignment-backend.vercel.app/todo?newData=${inputValue}`,{}, {
-          headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
-          }
-      });
-      alert(response.data.message)
-    }
+  //     const response = await axios.post(`https://w3dev-assignment-backend.vercel.app/todo?newData=${inputValue}`,{}, {
+  //         headers: {
+  //             Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
+  //         }
+  //     });
+  //     alert(response.data.message)
+  //   }
     
     
      
-    setInputValue('');
-  };
+  //   setInputValue('');
+  // };
 
-  const handleEdit = (text,id) => {
-    setInputValue(text);
-    setEditIndex(id);
-  };
-  const handleCheck = (id,checkstatus)=>{
-    const response = await axios.put(`https://w3dev-assignment-backend.vercel.app/todo/check/${id}/${checkstatus}`)
-    alert(response.data.message)
-  }
-  const handleDelete = async(id) => {
+  // const handleEdit = (text,id) => {
+  //   setInputValue(text);
+  //   setEditIndex(id);
+  // };
+  // const handleCheck = (id,checkstatus)=>{
+  //   const response = await axios.put(`https://w3dev-assignment-backend.vercel.app/todo/check/${id}/${checkstatus}`)
+  //   alert(response.data.message)
+  // }
+  // const handleDelete = async(id) => {
     
-    const response = await axios.delete(`https://w3dev-assignment-backend.vercel.app/todo/${id}`, {
-          headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
-          }
-      });
-      alert(response.data.message)
-  };
+  //   const response = await axios.delete(`https://w3dev-assignment-backend.vercel.app/todo/${id}`, {
+  //         headers: {
+  //             Authorization: `Bearer ${localStorage.getItem('token')}` // Include the token in the Authorization header
+  //         }
+  //     });
+  //     alert(response.data.message)
+  // };
 
   return (
     <div className="todo-container">
